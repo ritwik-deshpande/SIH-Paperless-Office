13f2d3a4a396738335b3aa5d76d35e4db0741858
ritwik <ritwikdeshpande01@gmail.com>
2020-03-26 13:03:01 +0530

    Title

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -142,7 +142,7 @@ export default function Dashboard() {
             <MenuIcon />
           </IconButton>
           <Typography component="h1" variant="h6" color="inherit"  noWrap className={classes.title}>
-            PAPERLESS OFFICE
+            DigiDocs
           </Typography>
           <IconButton color="inherit">
             <Badge badgeContent={4} color="secondary">
