7fc6f6b867598ad5c2619de37363cd84bf13a905
ritwik <ritwikdeshpande01@gmail.com>
2020-07-03 13:06:23 +0530

    Update Profile Page

--- src/Signatures/CreateESign.js
+++ src/Signatures/CreateESign.js
@@ -4,18 +4,19 @@ import styles from './styles.module.css'
 import useStyles from '../Style'
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-export default function CreateESign({userObj, saveSign}) {
+export default function CreateESign({userObj, saveUser}) {
 
   const classes = useStyles();
   let filereader
   let sigPad = {}
-
   const handleRead = (e) =>{
     console.log("Uploaded Esign is",filereader.result)
-    updateESign(filereader.result)
+    
 
   }
 
@@ -24,6 +25,10 @@ export default function CreateESign({userObj, saveSign}) {
     filereader = new FileReader()
     filereader.onloadend = handleRead
     filereader.readAsDataURL(file)
+  }
+  
+  const getSignImage = () =>{
+  	updateESign(filereader.result)
   }
 
   const clear = () => {
@@ -36,22 +41,80 @@ export default function CreateESign({userObj, saveSign}) {
     userObj.esign = sign
     console.log("The new esign is ",userObj.esign)
     console.log("The New User object",userObj.id)
+    alert("New Esignature Saved")
     //dispatch action
-    saveSign(userObj)
+    //saveSign(userObj)
+  }
+  
+  const updateUser = () =>{
+  
+  	console.log("The updated user is",userObj)
+  	saveUser(userObj)
+  
   }
   const  getSign = () => {
     
     updateESign(sigPad.getCanvas().toDataURL('image/png')) 
   }
+  const handleFieldChange = (event,key) => {
+	console.log("The value is",event.target.value,key)
+	userObj[key] = event.target.value
+ }
+
+ function getFields(){
+
+    let fields = []
+    for (var key in userObj){
+    	if(key.localeCompare("id") != 0  && key.localeCompare("username") && key.localeCompare("esign") && key.localeCompare("notifications") ){
+    		let field_key = key
+    	
+    		fields.push(<TextField
+		className={classes.margin}
+		id={key}
+		label={key}
+		style={{ margin: 20 }}
+		fullWidth
+          	margin="normal"
+		defaultValue={userObj[key]}
+		onChange={e => { handleFieldChange(e, field_key) }}
+	      />)
+    		
+    	}
+    
+    }
+    
+    return fields
+    	
+
+
+
+ }
 
   return (
     <div >
 
       <div className={classes.appBarSpacer} />
+	 <form className={classes.root} >
+	<div>
+	<Typography gutterBottom variant="h5" component="h1" style={{ margin: 20 }}>
+                UPDATE PROFILE
+        </Typography>
+        
+        {getFields()}
+ 
+	
+	</div>
+	</form>
 
-        <Grid container spacing={10}>
+        <Grid container spacing={10} style={{ margin: 20 }}>
+        <Typography gutterBottom variant="h5" component="h5">
+                To Update your ESignature you can either Sign on the Canvas or upload an Image of your ESignature
+              </Typography>
+	
           <Grid item xs={5}>
             <Card className={classes.cardDim}>
+
+		
 
               <Typography gutterBottom variant="h5" component="h2">
                 SIGN ON THE CANVAS
@@ -62,6 +125,7 @@ export default function CreateESign({userObj, saveSign}) {
               </div>
         
             </Card>
+            <br/>
             <Button variant="contained" color="primary" onClick={clear}>
               CLEAR CANVAS
             </Button>
@@ -70,7 +134,7 @@ export default function CreateESign({userObj, saveSign}) {
             <br/>
 
             <Button variant="contained" color="primary"  onClick={getSign}>
-              SAVE NEW ESIGNATURE
+              SAVE CANVAS ESIGNATURE
             </Button>
           </Grid>
 
@@ -79,27 +143,33 @@ export default function CreateESign({userObj, saveSign}) {
             {userObj.esign.localeCompare("Empty") == 0 ?  <Typography gutterBottom variant="h5" component="h2">
             ESIGNATURE  : ESIGN NOT CREATED
           </Typography> : <div><Typography gutterBottom variant="h5" component="h2">
-            UPDATED ESIGNATURE
+            CURRENT ESIGNATURE
           </Typography>
           <img src = {userObj.esign} alt ="Original Signature"></img>
           </div>
           }
             
         </Card>
+        <input label = "Upload Esignature Image" type = "file" accept =".png" onChange ={e => handleChange(e.target.files[0])}/>
+        <br/>
+        <Button variant="contained" color="primary"  onClick = {getSignImage} >
+          SAVE UPlOADED ESIGNATURE 
+        </Button>
         </Grid>
+        
+        <Button variant="contained" color="primary" onClick = { updateUser } >
+          UPDATE PROFILE 
+        </Button>
 
       </Grid>
+      
+      
 
       <div>
 
-      <br/>
         
-        <br/>
-        <br/>
-
-        <Button variant="contained" color="primary"  >
-          UPLOAD AND SAVE ESIGNATURE <input type = "file" accepct =".png" onChange ={e => handleChange(e.target.files[0])}/>
-        </Button>
+        
+	
         
         <br/>
         <br/>
