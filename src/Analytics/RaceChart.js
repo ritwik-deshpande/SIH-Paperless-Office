9a071ad4bc9d3cca0f835de8eade9eadc2652469
Ayush Singh <isayushsingh@gmail.com>
2020-07-30 01:25:17 +0530

    analytics fix, loading screen, tree card

--- src/Analytics/RaceChart.js
+++ src/Analytics/RaceChart.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { Helmet } from "react-helmet";
-const styles = theme => ({
-  numCardNumber: {
-		margin: "auto",
-    // flex: '1 0 auto',
-		padding: theme.spacing(1),
-		// alignItems: "flex-start",
-	},
-});
-
+import Iframe from 'react-iframe'
 export default function RaceChart() {
     
-    return(<div className="RaceChart">
-    <Helmet>
-      <script src="https://public.flourish.studio/resources/embed.js" />
-    </Helmet>
-    <div
-      class="flourish-embed flourish-bar-chart-race"
-      data-src="visualisation/3307459"
-      data-url="https://flo.uri.sh/visualisation/3307459/embed"
-    />
-  </div>
+    return(
+      <Iframe url="https://flo.uri.sh/visualisation/3307459/embed"
+    width="100%"
+    height="500px"/>
     )
 }
