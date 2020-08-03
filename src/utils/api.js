a524903b371996cb11bd5c321befbbfaaaa4b2f2
Ayush Singh <isayushsingh@gmail.com>
2020-08-03 11:22:32 +0530

    RadialChart for response time

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const Endpoint = "https://1c3817142143.ngrok.io"
-const Endpoint = "http://localhost:3030"
+const Endpoint = "https://c4c9f19299ad.ngrok.io"
+//const Endpoint = "http://localhost:3030"
 
 export default { 
 
